226096f01ea51cfc9f90665e7d58b497c82273fc
Kiss Tibor Peter <78260287+kisstp2006@users.noreply.github.com>
2024-09-11 09:44:20 +0200

    Add files via upload

--- Teszt/Program.cs
+++ Teszt/Program.cs
@@ -10,7 +10,26 @@ namespace Teszt
     {
         static void Main(string[] args)
         {
+            int Összeg = 0;
             //Teszt 
+            while(Összeg<100) { 
+                Console.WriteLine("Adjon meg számokat addig ameddig az összegük nem éri el a 100 at");
+                var szam = int.Parse(Console.ReadLine());
+                if (szam!=null) //Hogyha semmit nem írunk be nem fut le a program mivel rendes inputra vár
+                {
+                Console.WriteLine("A jelenleg megadott szám: "+szam.ToString());
+                Összeg += szam;
+                Console.WriteLine("Az eddigi összeg: "+Összeg);
+                    if (Összeg >= 100)
+                    {
+                        Console.WriteLine("Az eddigi összeg több mint 100: " + Összeg);
+                        Console.ReadKey();
+                        break;
+                    }
+                    
+                }
+            }
+
             Console.WriteLine("Szia, Hello világ");
         }
     }
